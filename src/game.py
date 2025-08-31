b4344100c9cf1cda57a235dee43a594182827067
estevan-ulian <estevan.ulian@gmail.com>
2025-08-31 11:34:31 -0300

    feat: implement Pipe class with collision detection and random pipe generation

--- src/game.py
+++ src/game.py
@@ -2,6 +2,7 @@ import pygame
 from src.constants import WINDOW_WIDTH, WINDOW_HEIGHT, FPS, GROUND_WIDTH
 from src.bird import Bird
 from src.ground import Ground
+from src.pipe import Pipe
 
 
 class Game:
@@ -12,13 +13,65 @@ class Game:
             'assets/sprites/background-day.png').convert_alpha()
         self.clock = pygame.time.Clock()
 
+        # Estados do jogo
+        self.MENU = 0
+        self.PLAYING = 1
+        self.GAME_OVER = 2
+        self.game_state = self.MENU
+
+        # Load start screen
+        self.title_image = pygame.image.load(
+            'assets/sprites/message.png').convert_alpha()
+
+        pygame.font.init()
+        self.font_large = pygame.font.Font(None, 28)
+        self.font_small = pygame.font.Font(None, 18)
+
     def is_off_screen(self, sprite):
+        """Check if a sprite is off the screen"""
         return sprite.rect[0] < -sprite.rect[2]
+
+    def draw_start_screen(self, background):
+        """Draw the start screen"""
+        self.window.blit(background, (0, 0))
+
+        title_rect = self.title_image.get_rect()
+        title_rect.center = (WINDOW_WIDTH // 2, WINDOW_HEIGHT // 3)
+        self.window.blit(self.title_image, title_rect)
+
+        instruction_text = self.font_small.render(
+            "Pressione ESPAÇO para começar", True, (255, 255, 255))
+        instruction_rect = instruction_text.get_rect()
+        instruction_rect.center = (WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2 + 80)
+        text_bg = pygame.Surface(
+            (instruction_rect.width + 20, instruction_rect.height + 10))
+        text_bg.fill((0, 0, 0))
+        text_bg.set_alpha(128)
+        text_bg_rect = text_bg.get_rect()
+        text_bg_rect.center = instruction_rect.center
+
+        self.window.blit(text_bg, text_bg_rect)
+        self.window.blit(instruction_text, instruction_rect)
+
+        controls_text = self.font_small.render(
+            "Pressione 'ESPAÇO' para voar", True, (255, 255, 255))
+        controls_rect = controls_text.get_rect()
+        controls_rect.center = (WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2 + 110)
+
+        controls_bg = pygame.Surface(
+            (controls_rect.width + 20, controls_rect.height + 10))
+        controls_bg.fill((0, 0, 0))
+        controls_bg.set_alpha(128)
+        controls_bg_rect = controls_bg.get_rect()
+        controls_bg_rect.center = controls_rect.center
+
+        self.window.blit(controls_bg, controls_bg_rect)
+        self.window.blit(controls_text, controls_rect)
 
     def start(self):
         background = pygame.transform.scale(
             self.background, (WINDOW_WIDTH, WINDOW_HEIGHT))
-        
+
         bird_group = pygame.sprite.Group()
         bird = Bird(100, 200)
         bird_group.add(bird)
@@ -28,38 +81,94 @@ class Game:
             ground = Ground(GROUND_WIDTH * i)
             ground_group.add(ground)
 
+        pipe_group = pygame.sprite.Group()
+
         while True:
-            # Set window title and icon
             pygame.display.set_caption("Flap Bird Game")
             pygame.display.set_icon(bird.images[0])
-
-            # Frame rate
             self.clock.tick(FPS)
 
-            # Quit event
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     quit()
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_SPACE:
-                        bird.bump()
+                        if self.game_state == self.MENU:
+                            self.game_state = self.PLAYING
+                            bird.rect.x = 100
+                            bird.rect.y = 200
+                            bird.speed = 0
+                            pipe_group.empty()
+                            for i in range(2):
+                                pipes = Pipe.get_random_pipes(
+                                    WINDOW_WIDTH * i + 800)
+                                pipe_group.add(pipes[0])
+                                pipe_group.add(pipes[1])
+                        elif self.game_state == self.PLAYING:
+                            bird.bump()
+                        elif self.game_state == self.GAME_OVER:
+                            self.game_state = self.MENU
 
-            # Draw background
-            self.window.blit(background, (0, 0))
-            
-            if self.is_off_screen(ground_group.sprites()[0]):
-                ground_group.remove(ground_group.sprites()[0])                
-                last_ground = ground_group.sprites()[-1]
-                new_ground = Ground(last_ground.rect.x + GROUND_WIDTH)
-                ground_group.add(new_ground)
+            match self.game_state:
+                case self.MENU:
+                    self.draw_start_screen(background)
+                case self.PLAYING:
+                    self.window.blit(background, (0, 0))
+                    if self.is_off_screen(ground_group.sprites()[0]):
+                        ground_group.remove(ground_group.sprites()[0])
+                        last_ground = ground_group.sprites()[-1]
+                        new_ground = Ground(last_ground.rect.x + GROUND_WIDTH)
+                        ground_group.add(new_ground)
 
-            # Draw ground           
-            ground_group.draw(self.window)
-            ground_group.update()
+                    if self.is_off_screen(pipe_group.sprites()[0]):
+                        pipe_group.remove(pipe_group.sprites()[0])
+                        pipe_group.remove(pipe_group.sprites()[0])
+                        pipes = Pipe.get_random_pipes(WINDOW_WIDTH * 2)
+                        pipe_group.add(pipes[0])
+                        pipe_group.add(pipes[1])
 
-            # Draw bird
-            bird_group.draw(self.window)
-            bird_group.update()
+                    ground_group.update()
+                    bird_group.update()
+                    pipe_group.update()
+
+                    bird_group.draw(self.window)
+                    pipe_group.draw(self.window)
+                    ground_group.draw(self.window)
+
+                    if (pygame.sprite.groupcollide(bird_group, ground_group, False, False, pygame.sprite.collide_mask)) or (pygame.sprite.groupcollide(bird_group, pipe_group, False, False, pygame.sprite.collide_mask)):
+                        pygame.mixer.Sound(
+                            'assets/audio/hit.ogg').play(loops=0, maxtime=0, fade_ms=0)
+                        self.game_state = self.GAME_OVER
+                case self.GAME_OVER:
+                    self.window.blit(background, (0, 0))
+                    self.window.blit(background, (0, 0))
+                    bird_group.draw(self.window)
+                    pipe_group.draw(self.window)
+                    ground_group.draw(self.window)
+
+                    game_over_text = self.font_large.render(
+                        "GAME OVER", True, (255, 255, 255))
+                    game_over_rect = game_over_text.get_rect()
+                    game_over_rect.center = (
+                        WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2)
+
+                    restart_text = self.font_small.render(
+                        "Pressione ESPAÇO para voltar ao menu", True, (255, 255, 255))
+                    restart_rect = restart_text.get_rect()
+                    restart_rect.center = (
+                        WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2 + 40)
+
+                    text_bg = pygame.Surface(
+                        (max(game_over_rect.width, restart_rect.width) + 40, 80))
+                    text_bg.fill((0, 0, 0))
+                    text_bg.set_alpha(180)
+                    text_bg_rect = text_bg.get_rect()
+                    text_bg_rect.center = (
+                        WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2 + 10)
+
+                    self.window.blit(text_bg, text_bg_rect)
+                    self.window.blit(game_over_text, game_over_rect)
+                    self.window.blit(restart_text, restart_rect)
 
             pygame.display.update()
--- src/pipe.py
+++ src/pipe.py
@@ -1,3 +1,36 @@
-class Pipe:
-    def __init__(self):
-        pass
+import pygame
+import random
+
+from src.constants import WINDOW_HEIGHT, PIPE_HEIGHT, PIPE_WIDTH, PIPE_GAP, GAME_SPEED
+
+
+class Pipe(pygame.sprite.Sprite):
+    def __init__(self, inverted, xpos, ysize):
+        pygame.sprite.Sprite.__init__(self)
+
+        self.image = pygame.image.load(
+            'assets/sprites/pipe-green.png').convert_alpha()
+        self.image = pygame.transform.scale(
+            self.image, (PIPE_WIDTH, PIPE_HEIGHT))
+
+        if inverted:
+            self.image = pygame.transform.flip(self.image, False, True)
+            self.rect = self.image.get_rect()
+            self.rect[0] = xpos
+            self.rect[1] = - (self.rect[3] - ysize)
+        else:
+            self.rect = self.image.get_rect()
+            self.rect[0] = xpos
+            self.rect[1] = WINDOW_HEIGHT - ysize
+
+        self.mask = pygame.mask.from_surface(self.image)
+
+    @staticmethod
+    def get_random_pipes(xpos):
+        size = random.randint(100, 300)
+        pipe = Pipe(False, xpos, size)
+        pipe_inverted = Pipe(True, xpos, WINDOW_HEIGHT - size - PIPE_GAP)
+        return (pipe, pipe_inverted)
+
+    def update(self):
+        self.rect[0] -= GAME_SPEED
